03d04fb6f5a4310178164afd4049b83e79fe65d2
lixumingwudi <lixumingwudi@sina.com>
2017-07-21 12:12:44 +0800

    增加了文章收藏功能

--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -16,7 +16,44 @@ Page({
     var postId = e.id;
     var postData = postsData.postList[postId];
     this.setData({
+      postId : postId,
       postData:postData
     });
+
+    //初始化图标显示的部分
+    var postsCollected = wx.getStorageSync('posts_Collected');
+    //如果缓存中存在postsCollected，便可对数据铏读取
+    if(postsCollected){
+      //读取缓存数据
+      var postCollected = postsCollected[postId];
+      //对前台的绑定数据进行更新
+      this.setData({
+        collected:postCollected
+      })
+    }else{
+      //否则，便说明postsCollected在缓存中不存在，那么，就需要先声明一个postsCollected对象出来，以便对以后数据进行存放
+      var postsCollected = {};
+      //将当前页面的图标状态默认为false
+      postsCollected[postId] = false;
+      //将数据再存入缓存中
+      wx.setStorageSync('posts_Collected',postsCollected);
+    }
+  },  //end of onLoad
+
+  collectTap: function(e){
+    var postId = this.data.postId;
+    //查看当前缓存中图标的状态，然后进行切换，再次更新之后传入前端
+    var postsCollected = wx.getStorageSync('posts_Collected');
+    var postCollected = postsCollected[postId];
+    //取反的作用是切换当前图标的状态
+    postCollected = !postCollected;
+    //将更新的数据传入到缓存对象当中
+    postsCollected[postId] = postCollected;
+    //更新缓存（更新后台数据）
+    wx.setStorageSync('posts_Collected',postsCollected);
+    //更新绑定的前台数据，这便是数据单向绑定的不便之处
+    this.setData({
+      collected: postCollected
+    })
   }
 });
--- pages/posts/posts.js
+++ pages/posts/posts.js
@@ -11,7 +11,6 @@ Page({
 
   onPostTap : function(e){
     var postId = e.currentTarget.dataset.postid;
-    //console.log(postId);
 
     wx.navigateTo({
       url:"post-detail/post-detail?id=" + postId
@@ -22,12 +21,10 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
     //利用setData进行数据绑定
     this.setData({
       posts_key:postsData.postList
     });
-
     //直接对data进行数据绑定
     //this.data.postList = postsData.postList;
   }
